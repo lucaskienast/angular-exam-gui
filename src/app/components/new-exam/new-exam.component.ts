7bc1bb7827b80f190ef2f950803553cb642e9a7f
LucasKienast <lucas.kienast@hotmail.de>
2022-06-27 23:43:10 +0200

    create exam form

--- src/app/components/new-exam/new-exam.component.ts
+++ src/app/components/new-exam/new-exam.component.ts
@@ -10,89 +10,93 @@ export class NewExamComponent implements OnInit {
 
   answerControlNames: string[] = ["answer1Control", "answer2Control", "answer3Control", "answer4Control"];
 
-  firstQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  secondQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  thirdQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  fourthQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  fifthQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  sixthQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  seventhQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  eigthQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  ninthQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  tenthQuestionFormGroup = this._formBuilder.group({
-    questionControl: ['', Validators.required],
-    correctAnswerControl: ['', Validators.required],
-    answer1Control: ['', Validators.required],
-    answer2Control: ['', Validators.required],
-    answer3Control: ['', Validators.required],
-    answer4Control: ['', Validators.required],
-  });
-  userCredentialsFormGroup = this._formBuilder.group({
-    usernameControl: ['', Validators.required],
-    passwordControl: ['', Validators.required],
+  createExamForm = this._formBuilder.group({
+    userFormGroup: this._formBuilder.group({
+      usernameControl: ['', Validators.required],
+      passwordControl: ['', Validators.required]
+    }),
+    questionFormGroups: this._formBuilder.group({
+      firstQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      secondQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      thirdQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      fourthQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      fifthQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      sixthQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      seventhQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      eigthQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      ninthQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+      tenthQuestionFormGroup: this._formBuilder.group({
+        questionControl: ['', Validators.required],
+        correctAnswerControl: ['', Validators.required],
+        answer1Control: ['', Validators.required],
+        answer2Control: ['', Validators.required],
+        answer3Control: ['', Validators.required],
+        answer4Control: ['', Validators.required],
+      }),
+    })
   });
 
   isLinear = false;
@@ -107,7 +111,7 @@ export class NewExamComponent implements OnInit {
   }
 
   submitExamCreation(): void {
-    console.log(this.userCredentialsFormGroup);
+    console.log(this.createExamForm);
   }
 
 }
